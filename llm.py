384a15c4d64054d60a82858f48120dd623144d7d
Sampurnaa <nsampurnaa@gmail.com>
2025-08-30 23:35:33 +0530

    llm.py

--- llm.py
+++ llm.py
@@ -1,19 +1,18 @@
-from huggingface_hub import InferenceClient
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import os
 
-client = InferenceClient(
-    model="mistralai/Mistral-7B-Instruct-v0.2",
-    token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, token=os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+model = AutoModelForCausalLM.from_pretrained(model_name, token=os.getenv("HUGGINGFACEHUB_API_TOKEN"))
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
 )
 
-def ask_llm(prompt: str) -> str:
-    try:
-        response = client.text_generation(
-            prompt,
-            max_new_tokens=200,
-            do_sample=True,
-            temperature=0.7
-        )
-        return response
-    except Exception as e:
-        return f"Error: {str(e)}"
+def ask_llm(prompt):
+    response = pipe(prompt, max_new_tokens=200, do_sample=True, temperature=0.7)
+    return response[0]["generated_text"]
